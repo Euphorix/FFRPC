10cdd3a0d4d23b90e67030f2cba7d039cb57ac28
Evan Zhao <zxfown@gmail.com>
2013-06-25 18:38:15 -0700

    add more test code

--- example/redrabbit/main.cpp
+++ example/redrabbit/main.cpp
@@ -9,27 +9,32 @@
 #include "base/log.h"
 #include "rpc/ffscene_python.h"
 #include "rpc/ffgate.h"
+#include "base/signal_helper.h"
+#include "base/daemon_tool.h"
 
 using namespace ff;
-//./example/redrabbit/app_redrabbit -gate gate@0 -broker tcp://127.0.0.1:10241 -gate_listen tcp://127.0.0.1:10242 -python_path /home/evan/work/ffrpc -scene scene@0
+//./example/redrabbit/app_redrabbit -gate gate@0 -broker tcp://127.0.0.1:10241 -gate_listen tcp://121.199.21.238:10242 -python_path /home/evan/work/ffrpc -scene scene@0
 
 int main(int argc, char* argv[])
 {
+    arg_helper_t arg_helper(argc, argv);
     //! 美丽的日志组件，shell输出是彩色滴！！
     LOG.start("-log_path ./log -log_filename log -log_class XX,BROKER,FFRPC,FFGATE,FFSCENE,FFSCENE_PYTHON -log_print_screen true -log_print_file true -log_level 6");
 
-    //! 启动broker，负责网络相关的操作，如消息转发，节点注册，重连等
     ffbroker_t ffbroker;
-    ffbroker.open("-l tcp://127.0.0.1:10241");
-
     ffgate_t ffgate;
     ffscene_python_t ffscene_python;
+
+    if (arg_helper.is_enable_option("-d"))
+    {
+        daemon_tool_t::daemon();
+    }
+
+    //! 启动broker，负责网络相关的操作，如消息转发，节点注册，重连等
+    ffbroker.open(string("-l ") + arg_helper.get_option_value("-broker"));
     
-    arg_helper_t arg_helper(argc, argv);
     try
     {
-        sleep(1);
-
         if (ffgate.open(arg_helper))
         {
             printf("gate open error!\n");
@@ -41,17 +46,19 @@ int main(int argc, char* argv[])
         {
             sleep(1);
             printf("scene open error!\n");
-            
             return 0;
         }
-
-        
     }
     catch(exception& e_)
     {
         printf("exception=%s\n", e_.what());
     }
-    sleep(300);
+    signal_helper_t::wait();
+
+    ffgate.close();
+    ffscene_python.close();
     ffbroker.close();
+    net_factory_t::stop();
+    sleep(1);
     return 0;
 }
