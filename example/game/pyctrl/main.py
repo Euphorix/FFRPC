278dae157bccdb6419bb892f617ecbfef0a98f70
Evan Zhao <zxfown@gmail.com>
2014-03-05 06:39:42 -0800

    mmore for code

--- example/game/pyctrl/main.py
+++ example/game/pyctrl/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import ffext
-
+import json
 from msg.ffengine import ttypes 
 #from msg_def import ttypes
 
@@ -31,6 +31,7 @@ def process_echo(ffreq):
     ret_msg = ttypes.process_cmd_ret_t(1, '暂未开放此功能')
     ret_msg.ret_code = 1
     ret_msg.output_msg = ''
+    ret_msg.info = {}
     if ffreq.msg.cmd == '查看状态':
         ret_msg.output_msg = '关闭'
     elif ffreq.msg.cmd == '启动':
@@ -41,6 +42,10 @@ def process_echo(ffreq):
         if ffreq.msg.param != None:
             for k, v in ffreq.msg.param.items():
                 f.write(k + ' ' + v + '\n')
+            tmpS = "-log_path ./%s/log -log_filename log "%(ffreq.msg.process_name)+\
+                   "-log_class DB_MGR,XX,BROKER,FFRPC,FFGATE,FFSCENE,FFSCENE_PYTHON,FFNET "+\
+                   "-log_print_screen true -log_print_file true -log_level 6"
+            f.write(tmpS+'\n')
             if ffreq.msg.param.get('-scene') != None:
                 f.write('-python_path ./%s/pyproject \n'%(ffreq.msg.process_name))
         f.close()
@@ -51,8 +56,10 @@ def process_echo(ffreq):
             if ps_app(ffreq.msg.process_name):
                 ret_msg.output_msg = ffreq.msg.process_name +'启动成功'
             else:
-                ret_msg.output_msg = ffreq.msg.process_name +'关闭失败'
+                ret_msg.ret_code = 0
+                ret_msg.output_msg = ffreq.msg.process_name +'启动失败'
         else:
+            ret_msg.ret_code = 0
             ret_msg.output_msg = ffreq.msg.process_name +'启动失败'
     elif ffreq.msg.cmd == '关闭':
         pid = ps_app(ffreq.msg.process_name)
@@ -66,6 +73,10 @@ def process_echo(ffreq):
     elif ffreq.msg.cmd == '更新代码':
         print(ffreq.msg.param['svn_url'])
         ret_msg.output_msg = '代码更新成功:\n'+update_code(ffreq.msg.process_name, ffreq.msg.param['svn_url'])
+    elif ffreq.msg.cmd == '查看日志':
+        ret_msg.info['update_data'] = dump_path(ffreq.msg.process_name + '/log/'+ffreq.msg.param['path'])
+    elif ffreq.msg.cmd == '查看代码':
+        ret_msg.info['update_data'] = dump_path(ffreq.msg.process_name + '/pyproject/'+ffreq.msg.param['path'])   
     else:
         ret_msg.ret_code = 0
         ret_msg.output_msg = '当前后台不支持此操作'
@@ -120,6 +131,54 @@ def update_code(app_name, url):
         ver = a[len(a) -1]
         return ver
     return ""
+def dump_path(path):
+    #print('process_dump', ffreq.msg)
+    ret_msg = {}
+    ret_msg['flag'] = 1 #dir =1， file=2
+    ret_msg['dir']  = []
+    ret_msg['data'] = ''
+    prefix = "./"
+    path_name = prefix + path
+    print(path_name)
+    raw_flag = False
+    if path_name[-1:] == '@':
+        raw_flag = True
+        path_name = path_name[0:len(path_name)-1]
+    if os.path.isdir(path_name):
+        list_files = os.listdir(path_name)
+        for k in list_files:
+            if k != '' and k[0:1] != '.' and k[0:4] != 'app_' and k[-2:] != '.o' and k[-4:] != '.pyc':
+                ret_msg['dir'].append(k)
+    elif os.path.isfile(path_name):
+        ret_msg['flag'] = 2
+        pre_code = ''
+        end_code = ''
+        f = open(path_name, 'r')
+        
+        #if path_name[-4:] == '.lua':
+        #    pre_code = '<textarea  cols="80" rows="20" class="pane"><pre class=\'brush: lua\'>'
+        #    end_code = '</pre></textarea>'
+        #elif path_name[-3:] == '.py':
+        #    pre_code = '<textarea  cols="80" rows="20" class="pane"><pre class=\'brush: python\'>'
+        #    end_code = '</pre></textarea>'
+        data = pre_code
+        for line in f.readlines():
+           if raw_flag:
+               data += line
+           else:
+               data += line #+ "<br/>" 
+        f.close()
+        if data[-1:] == '\n':
+            data = data[0:-1]
+        data += end_code
+        ret_msg['data'] = data
+    else:
+        ret_msg['flag'] = 0
+	
+    #print('process_dump ret_msg', ret_msg)
+    tmpJson = json.dumps(ret_msg, ensure_ascii=False)
+    print('process_dump ret_msg', tmpJson)
+    return tmpJson
 
 #@ffext.ff_reg_scene_interfacee(ttypes.dump_file_req_t, ttypes.dump_file_ret_t)
 def process_dump(ffreq):
